e0abe1957ef7be1e2cfe0dd607b36ce5408498e4
Dylan Thomas <dylan.thomas@gmail.com>
2023-03-21 10:00:25 -0500

    cleanup for website branch

--- preload.js
+++
@@ -1,7 +0,0 @@
-/**
- * The preload script runs before. It has access to web APIs
- * as well as Electron's renderer process modules and some
- * polyfilled Node.js functions.
- * 
- * https://www.electronjs.org/docs/latest/tutorial/sandbox
- */
